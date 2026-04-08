3690bf8d1f38102274a5206cd8ec9477efdb13b3
pranitmaske18 <pranitmaske2006@gmail.com>
2026-04-09 00:54:31 +0530

    simle function

---
+++ Function n Methods/simplefunction.java
@@ -0,0 +1,9 @@
+public class simplefunction {
+    public static void greet(){
+        System.out.println("Hello World");
+        return;
+    }
+    public static void main(String[] args) {
+        greet();
+    }
+}
