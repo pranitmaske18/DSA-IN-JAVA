a7a59a4e9dc987dd6cc3e050a902213b5153d010
pranitmaske18 <pranitmaske2006@gmail.com>
2026-07-15 23:32:33 +0530

    learn hollow pattern printing

---
+++ PatternPartII/Horec.java
@@ -0,0 +1,23 @@
+package PatternPartII;
+
+public class Horec {
+    public static void hrec(int totrows,int totcols){
+        for(int i=1; i<=totrows;i++){
+            for(int j=1; j<=totcols;j++){
+                if(i==1 || i==totrows || j==1 || j==totcols){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        
+    }
+
+public static void main(String args[]){
+        hrec(4,5);
+    }  
+}
+
